cee69d65bcfbbd136a320edbe742e97b6238b07e
闫逍旭 <yanxx@shinemo.com>
2016-06-16 06:37:04 +0800

    Initial Commit

---
+++ mpush_client/mpush_client.h
@@ -0,0 +1,19 @@
+//
+//  mpush_client.h
+//  mpush_client
+//
+//  Created by ohun on 16/6/16.
+//  Copyright © 2016年 mpusher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mpush_client.
+FOUNDATION_EXPORT double mpush_clientVersionNumber;
+
+//! Project version string for mpush_client.
+FOUNDATION_EXPORT const unsigned char mpush_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mpush_client/PublicHeader.h>
+
+
